c67d10ca2ffacee1078cf9792c3721014c30c59d
Biraj Mainali <biraj.prathamit@gmail.com>
2021-06-17 20:29:13 +0545

    string Assignment

---
+++ String Assignment/copy one string to another string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char original[1000], Copy[1000];
+
+    printf("Input a string\n");
+    gets(source);
+
+    strcpy(Copy, original);
+
+    printf("original string: %s\n", original);
+    printf("Copy string: %s\n", Copy);
+
+    return 0;
+}
---
+++ String Assignment/to convert lowercase string to uppercase..c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100 // Maximum string size
+
+
+int main()
+{
+    char str[MAX_SIZE];
+
+    printf("Enter your text : ");
+    gets(str);
+
+    strupr(str);
+
+    printf("Uppercase string : %s", str);
+
+    return 0;
+}
